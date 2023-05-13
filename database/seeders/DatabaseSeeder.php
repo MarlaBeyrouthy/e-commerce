4202d8099624e5b00622079e80257682a5e0af09
HamzaMuhareb <107720210+HamzaMuhareb@users.noreply.github.com>
2023-05-13 11:29:32 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,10 +10,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
         $this->call([
             ColorsTableSeeder::class,
+            UserSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
