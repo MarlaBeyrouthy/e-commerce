bab46aa21bfdf2dc9325c7c172a79fdf78e13e79
HamzaMuhareb <107720210+HamzaMuhareb@users.noreply.github.com>
2023-07-05 13:43:52 +0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,21 +16,37 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([[
+            'name' => 'admin',
+            'email' =>'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'address' => 'none',
+            'phone' =>'0000',
+            'contact' => 'none',
+            'permission_id'=>2,
+        ],[
             'name' => 'hamza',
             'email' =>'hhh@gmail.com',
             'password' => bcrypt('12341234'),
             'address' => 'damas',
             'phone' =>'09111111',
-            //'verified'=>'1',
             'contact' => 'hello',
+            'permission_id'=>1,
         ],[
             'name' => 'marla',
             'email' =>'mmm@gmail.com',
             'password' => bcrypt('12341234'),
             'address' => 'damas',
             'phone' =>'09111111',
-            //'verified'=>'1',
             'contact' => 'hello',
+            'permission_id'=>1,
+        ],[
+            'name' => 'banned_user',
+            'email' =>'banned@gmail.com',
+            'password' => bcrypt('12341234'),
+            'address' => 'damas',
+            'phone' =>'09111111',
+            'contact' => 'hello',
+            'permission_id'=>3,
         ]]);
         //
     }
